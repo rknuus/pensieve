7dc0b5c9b8de6f53bc5d4fe111a1dce58c2094b5
Raphael Knaus <rknuus@gmail.com>
2022-06-17 15:20:05 +0200

    Add three demo boxes

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Box from './Box/Box.svelte';
   import Stack from './Stack/Stack.svelte';
 </script>
 
@@ -8,3 +9,7 @@
 <Stack id="stackA" --top="50px" --left="50px" />
 <Stack id="stackB" --top="50px" --left="450px"  />
 <Stack id="stackC" --top="50px" --left="850px"  />
+
+<Box id="boxA" --top="250px" --left="50px" />
+<Box id="boxB" --top="250px" --left="450px"  />
+<Box id="boxC" --top="250px" --left="850px"  />
---
+++ src/Box/Box.svelte
@@ -0,0 +1,22 @@
+<script>
+  import OpenCards from './OpenCards.svelte';
+  import FlippedCards from './FlippedCards.svelte';
+
+  export let id;
+
+  console.assert(id, 'box has no valid ID');
+</script>
+
+<style>
+  .box {
+    position: absolute;
+    /* borrowed from https://svelte.dev/repl/ccdb128d448c4b92babeaccb4be35567?version=3.46.2 */
+    top: var(--top);
+    left: var(--left);
+  }
+</style>
+
+<div class="box">
+  <OpenCards id="{id}" />
+  <FlippedCards  id="{id}" />
+</div>
