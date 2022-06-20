81b1c15e426493a4334708eb34d32ef175761696
Raphael Knaus <rknuus@gmail.com>
2022-06-20 22:57:25 +0200

    Calculate size of stacks based on child cards
    Because the cards are positioned absolutely, the parent div sizes are
    not automatically set to contain all children, but are 0, 0.
    
    Exposing properties as CSS variable is borrowed from
    https://svelte.dev/repl/14a1b548093642a9a2dfb3e615382732?version=3.48.0.
    But as our interest is limited to width and height, we hard code the
    variables in the setter functions, unlike the original code, which is
    more generic.
    
    KLUDGE(KNR): The card dimensions are currently hard coded in the stack
    size functions. If possible they should be queries from a Card component
    or read from a store. This will be fixed in a future commit.
    
    The same approach will be applied to other container components like Box
    in a future commit.
    
    BEWARE(KNR): It's unclear whether the CSS variables will be updated,
    should the size change dynamically. It all depends on whether the stack
    div will be redrawn and whether the CSS setters are called in the
    process.

--- src/Stack/Stack.svelte
+++ src/Stack/Stack.svelte
@@ -9,6 +9,9 @@
 
   let cards;
 
+  $: width = getWidth(cards);
+  $: height = getHeight(cards);
+
   const stack_offset_factor = 5;
   const dispatch = createEventDispatcher();
 
@@ -34,6 +37,37 @@
     display.stopDragging();
     console.debug('dropped item ' + eventData.cardId + ' onto target stack ' + id);
   }
+
+  function getCardOffset(stackLevel) {
+    return stackLevel * stack_offset_factor;
+  }
+
+  function getHeight(cards) {
+    if (cards.length === 0) {
+      return 0;
+    }
+    // TODO(KNR): should we move card size to the display-store?!
+    const cardHeight = 112;
+    return cardHeight + getCardOffset(cards.length);
+  }
+
+  function getWidth(cards) {
+    if (cards.length === 0) {
+      return 0;
+    }
+    // TODO(KNR): should we move card size to the display-store?!
+    const cardHeight = 200;
+    return cardHeight + getCardOffset(cards.length);
+  }
+
+  // exposing height as CSS variable is borrowed from https://svelte.dev/repl/14a1b548093642a9a2dfb3e615382732?version=3.48.0
+  function setCssHeight(node) {
+    node.style.setProperty(`--height`, height);
+  }
+
+  function setCssWidth(node) {
+    node.style.setProperty(`--width`, width);
+  }
 </script>
 
 <style>
@@ -42,12 +76,14 @@
     /* borrowed from https://svelte.dev/repl/ccdb128d448c4b92babeaccb4be35567?version=3.46.2 */
     top: var(--top);
     left: var(--left);
+    width: calc(var(--width) * 1px);
+    height:  calc(var(--height) * 1px);
   }
 </style>
 
-<div class="stack">
+<div class="stack" use:setCssHeight use:setCssWidth>
   {#each cards as cardId, i}
-    <Card id={cardId} parentId={id} topCard={i === cards.length - 1} draggable={true} --top="{i * stack_offset_factor}px" --left="{i * stack_offset_factor}px" />
+    <Card id={cardId} parentId={id} topCard={i === cards.length - 1} draggable={true} --top="{getCardOffset(i)}px" --left="{getCardOffset(i)}px" />
   {/each}
-  <CardDropZone parentId={id} showAlways="{cards.length === 0}" --top="{cards.length * stack_offset_factor}px" --left="{cards.length * stack_offset_factor}px" on:drop="{onDrop}" />
+  <CardDropZone parentId={id} showAlways="{cards.length === 0}" --top="{getCardOffset(cards.length)}px" --left="{getCardOffset(cards.length)}px" on:drop="{onDrop}" />
 </div>
