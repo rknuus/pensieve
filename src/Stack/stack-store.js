61b810601f138a261ebe10acc930c7e7f381e5b6
Raphael Knaus <rknuus@gmail.com>
2022-07-23 15:07:02 +0200

    Support adding and removing cards from a stack in the store
    To avoid redundant code reuse the new addCard and removeCard functions
    in moveCard. However, I'm not sure a store update should be atomic. With
    this refactoring that's no longer the case...

--- src/Stack/stack-store.js
+++ src/Stack/stack-store.js
@@ -17,16 +17,31 @@ const store = writable([
   ]);
 
 function moveCard(cardId, sourceId, targetId) {
+  // TODO(KNR): not sure it's an issue if the move is not atomic
+  removeCard(cardId, sourceId);
+  addCard(cardId, targetId);
+}
+
+function removeCard(cardId, sourceId) {
   store.update(items => {
     const sourceIndex = items.findIndex(s => s.id === sourceId);
     console.assert(sourceIndex !== -1);
+
+    const updatedItems = [...items];
+    console.assert(updatedItems[sourceIndex].cards.includes(cardId));
+    updatedItems[sourceIndex].cards = updatedItems[sourceIndex].cards.filter(c => c !== cardId);
+
+    return updatedItems;
+  });
+}
+
+function addCard(cardId, targetId) {
+  store.update(items => {
     const targetIndex = items.findIndex(s => s.id === targetId);
     console.assert(targetIndex !== -1);
 
     const updatedItems = [...items];
-    console.assert(updatedItems[sourceIndex].cards.includes(cardId));
     console.assert(!updatedItems[targetIndex].cards.includes(cardId));
-    updatedItems[sourceIndex].cards = updatedItems[sourceIndex].cards.filter(c => c !== cardId);
     updatedItems[targetIndex].cards.push(cardId);
 
     return updatedItems;
@@ -40,4 +55,5 @@ export const stacks = {
   update: (id, data) => { updateItemInStore(id, data, store); },
   remove: (id) => { removeItemFromStore(id, store); },
   moveCard: (cardId, sourceId, targetId) => { moveCard(cardId, sourceId, targetId); },
+  removeCardFromStack: (cardId, sourceId) => { removeCard(cardId, sourceId); },
 };
